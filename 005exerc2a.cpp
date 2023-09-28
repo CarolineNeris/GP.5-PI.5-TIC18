d963a4348be8a68e33a3352c188336089f08ee0e
Lasbernardes <lasbernardes@uesc.br>
2023-09-28 10:47:21 -0300

    v3 005exerc2c.cpp

--- 005exerc2a.cpp
+++ 005exerc2a.cpp
@@ -8,5 +8,12 @@ int main(){
 
     cout<<"Digite um caracter: ";
     cin>>ch1;
+
+    //cout<<
+        ch1>='A'&&ch1<='Z'?cout<<"Maiusculo"<<endl:
+            ch1>='a'&&ch1<='z'?cout<<"Minusculo"<<endl:
+                ch1>'Z'&&ch1<'a'||ch1>'z'?cout<<"Um digito"<<endl:
+                    cout<<"outro tipo de caracter"<<endl;
+    //<<endl;
     return 0;
 }
