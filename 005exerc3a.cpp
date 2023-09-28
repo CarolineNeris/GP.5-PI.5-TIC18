fc8fc4e23eed97db9efe005a8e4a633c3c63529f
Lasbernardes <lasbernardes@uesc.br>
2023-09-28 11:31:58 -0300

    v2 005exerc3b.cpp

--- 005exerc3a.cpp
+++ 005exerc3a.cpp
@@ -5,7 +5,12 @@ using namespace std;
 int main(){
 
     int a,b,c;
-    
+
+    cout<<"Digite dois valores inteiros: ";
+    cin>>a>>b;
+
+    cout<<a<<endl;
+    cout<<b<<endl;
 
     return 0;
 }
