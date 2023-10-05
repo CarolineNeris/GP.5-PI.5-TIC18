34f0f1535462c111f2a39ce05dc0ebac52b8e07d
Matheus Beckerath <matheus.beckerath@hotmail.com>
2023-10-05 11:01:12 -0300

    Adicionando 3b

---
+++ exercicio3.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+
+int main(void){
+    int a, b, c;
+
+    cout << "Digite um numero inteiro : " << endl;
+    cin >> a;
+    cout << "Digite outro numero inteiro : " << endl;
+    cin >> b;
+    
+    
+    return 0;
+}
