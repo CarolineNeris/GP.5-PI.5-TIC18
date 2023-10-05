fc0fd334e630b22d5dd752091916d3e22a765e85
Matheus Beckerath <matheus.beckerath@hotmail.com>
2023-10-05 12:33:14 -0300

    Adicionando 5a

---
+++ exercicio5.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // Letra a 
+    unsigned char informacao_genetica;
+    cout << "Digite a informação genética da planta (um número entre 0 e 255): ";
+    cin >> informacao_genetica;
+
+    
+
+    return 0;
+}
